226717a56417107a2f70d58e25d6b325434b37a8
josefurtado <josefurtado.digital@gmail.com>
2021-05-07 18:14:08 +0200

    Home - All media queries for tablet done

--- dist/main.07544d9b.js
+++ dist/main.07544d9b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\assets\\home\\desktop\\image-hero-coffeepress.jpg":[["image-hero-coffeepress.145df461.jpg","assets/home/desktop/image-hero-coffeepress.jpg"],"assets/home/desktop/image-hero-coffeepress.jpg"],"./..\\assets\\home\\tablet\\image-hero-coffeepress.jpg":[["image-hero-coffeepress.3c069112.jpg","assets/home/tablet/image-hero-coffeepress.jpg"],"assets/home/tablet/image-hero-coffeepress.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63357" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60805" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

f55348a27be88f09febb9e9400c0a51a7592b993
josefurtado <josefurtado.digital@gmail.com>
2021-05-07 11:52:54 +0200

    Home- Setup all the links and disable user select for images, links, and logo text

--- dist/main.fb6bbcaf.js
+++ dist/main.fb6bbcaf.js
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57323" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63357" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

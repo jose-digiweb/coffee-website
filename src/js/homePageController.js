de4c478b0d13a54a06ae1e01c4555b1a3fa421c3
jose-digiweb <josefurtado.digital@gmail.com>
2021-08-07 15:02:57 +0200

    created another controllerJs module for home page

---
+++ src/js/homePageController.js
@@ -0,0 +1,22 @@
+//--> Transcribe and Polyfill
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+//--> Importing modules
+import mobileMenuView from './views/mobileMenuView.js';
+
+const mobileMenuControl = function () {
+  //--> Show and Hide Mobile Btn
+  mobileMenuView.mobileMenuBtn();
+
+  //--> Open Mobile Menu
+  mobileMenuView.openMenu();
+
+  //--> Close Mobile Menu
+  mobileMenuView.closeMenu();
+};
+
+const init = function () {
+  mobileMenuControl();
+};
+init();
